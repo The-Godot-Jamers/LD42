cda2e366bfd242d8a710f26a1d756fe10a1dba8a
Plopsis <kirvesmies70@hotmail.com>
2018-08-11 07:52:02 +0300

    sounds

--- scripts/game.gd
+++ scripts/game.gd
@@ -12,13 +12,14 @@ func _physics_process(delta):
 	if position.y >= bottom:
 		Globals.GUI.hide()
 		get_tree().change_scene_to(load("res://scenes/gameover.tscn"))
+		$wompwomp.play()
 
 func _on_pickup_area_body_entered(body):
 	if not picked:
 		Globals.score += 1
 		Globals.GUI.update_gui(Globals.score)
 		$AnimationPlayer.play("bling")
-		$AudioStreamPlayer.play()
+		$bling.play()
 		picked = true
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
--- scripts/startup.gd
+++ scripts/startup.gd
@@ -1,7 +1,10 @@
 extends Node
 
 
-
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		_on_TextureButton_pressed()
 func _on_TextureButton_pressed():
+	$AudioStreamPlayer.play()
 	Globals.GUI.startup()
 	get_tree().change_scene_to(load("res://scenes/container.tscn"))
