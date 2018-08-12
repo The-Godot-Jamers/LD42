96837644b5dead8d7f5b54faff4ba573b3f9af2e
Plopsis <kirvesmies70@hotmail.com>
2018-08-12 12:11:24 +0300

    fix for enemies getting stuck on lvl 2

--- scripts/level2.gd
+++ scripts/level2.gd
@@ -12,4 +12,13 @@ func _ready():
 	for z in 15:
 		z = load("res://scenes/enemy1.tscn").instance()
 		z.position = Vector2(rand_range(50,3000),rand_range(-500,500))
+		var pos = $TileMap.world_to_map(z.position)
+		pos.x -=1
+		pos.y -=1
+		for xx in 3:
+			for yy in 3:
+				if $TileMap.get_cellv(pos) != -1:
+					$TileMap.set_cellv(pos,-1)
+				pos.y +=1
+			pos.x +=1
 		add_child(z)
