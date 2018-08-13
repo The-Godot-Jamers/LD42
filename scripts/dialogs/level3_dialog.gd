d0066fbdc8d953303b0a798e73c65a6fbfe67127
Tittle Pending <Burningwoodmedia@gmail.com>
2018-08-14 01:39:11 +0900

    Merge branch 'master' of https://github.com/The-Godot-Jamers/LD42

---
+++ scripts/dialogs/level3_dialog.gd
@@ -0,0 +1,41 @@
+extends Node
+
+func _ready():
+	Ren.connect("story_step", self, "story")
+	Ren.jump(
+		"intro",
+		name,
+		str(0),
+		false
+	)
+	Ren.start()
+	Globals.active_ren()
+
+func portal_text():
+	Ren.story_state = str(1)
+	Ren.story_step()
+	Globals.active_ren()
+
+func story(dialog_name):
+	if dialog_name != name:
+		return
+
+	if Ren.current_node != self:
+		Ren.current_node = self
+
+	match Ren.story_state:
+		"0":
+			Ren.say({
+			"who":"dream",
+			"what": """
+				This is Gabe Renewal. He is your God!
+				"""
+			})
+		"1":
+			Ren.say({
+			"who":"dream",
+			"what": """
+				Get to the portal.
+				"""
+			})
+
