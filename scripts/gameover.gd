60e6d08fa4613052066f9e4c3cf7d9f4a18aa022
Plopsis <kirvesmies70@hotmail.com>
2018-08-13 19:20:45 +0300

    gameover screen fix & lvl2 portal behind tile fix

--- scripts/gameover.gd
+++ scripts/gameover.gd
@@ -1,7 +1,7 @@
 extends "res://scripts/startup.gd"
 
 func _ready():
-	$CenterContainer/VBoxContainer/score.text = str("You collected ",str(Globals.score)," games")
+	$CenterContainer/VBoxContainer/score.text = str("You collected ",str(Globals.score + Globals.killed)," games")
 
 
 
--- scripts/level2.gd
+++ scripts/level2.gd
@@ -10,14 +10,14 @@ func _process(delta):
 
 func _ready():
 	for i in 40:
-		var xx = rand_range(5,98)
+		var xx = rand_range(5,93)
 		var yy = rand_range(-16,16)
 		$TileMap.set_cell(xx,yy,0)
 		$TileMap.set_cell(xx-1,yy,0)
 		$TileMap.set_cell(xx+1,yy,0)
 	for z in 15:
 		z = load("res://scenes/enemy1.tscn").instance()
-		z.position = Vector2(rand_range(100,3000),rand_range(-500,500))
+		z.position = Vector2(rand_range(150,3000),rand_range(-500,500))
 		add_child(z)
 		var pos = $TileMap.world_to_map(z.position)
 		pos.x -=1
